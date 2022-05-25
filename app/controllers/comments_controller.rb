7186cb1d932ffdb360998a02031ec6a4cc7c1185
SuebSaifi <sueb18-cs@danskar.org>
2022-05-25 16:50:06 +0530

    not auth

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-    http_basic_authenticate_with name:"sueb",password:'sueb' , only: :destroy
+    # http_basic_authenticate_with name:"sueb",password:'sueb' , only: :destroy
 def create
 
     @article = Article.find(params[:article_id])
